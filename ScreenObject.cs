e75f2b0b090bd5e5ef68e4c6a73490d468b9aafd
ab <andre.berlemont@gmail.com>
2017-09-29 18:21:08 +0200

    generic screen management

---
+++ ScreenObject.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenObject : EngineObject
+{
+  public bool sticky = false;
+  protected Canvas[] _canvas;
+
+  protected override void build()
+  {
+    base.build();
+
+    fetchData();
+
+    if (_canvas == null) Debug.LogError("no canvas ?");
+    
+    //if (_canvas == null) Debug.LogError("wat ?");
+  }
+
+  public override void onEngineSceneLoaded()
+  {
+    base.onEngineSceneLoaded();
+    
+    hide();
+  }
+
+  [ContextMenu("fetch")]
+  override protected void fetchData()
+  {
+    base.fetchData();
+    _canvas = transform.GetComponentsInChildren<Canvas>();
+  }
+
+  protected void toggleVisible(bool flag)
+  {
+    if(!Application.isPlaying) fetchData();
+
+    if (sticky) flag = true;
+
+    for (int i = 0; i < _canvas.Length; i++)
+    {
+      //Debug.Log(name + "  " + _canvas[i].name);
+
+      if (_canvas[i].enabled != flag) _canvas[i].enabled = flag;
+    }
+
+    //Debug.Log(name+" , "+flag, gameObject);
+  }
+
+  [ContextMenu("show")]
+  virtual public void show()
+  {
+    //ScreensManager.get().call("");
+    if(ScreensManager.get() != null) ScreensManager.get().killAll();
+
+    toggleVisible(true);
+  }
+
+  public void showAll()
+  {
+    toggleVisible(true);
+  }
+  
+  [ContextMenu("hide")]
+  virtual public void hide()
+  {
+    if (sticky) return;
+    toggleVisible(false);
+  }
+
+  public bool isVisible()
+  {
+    return _canvas[0].enabled;
+  }
+
+  public void act_call_home()
+  {
+    call_home();
+  }
+
+  static public void call_home() {
+    //stop behaviors
+    ArenaSnakeManager asm = (ArenaSnakeManager)ArenaSnakeManager.get();
+    if(asm != null)
+    {
+      asm.kill();
+    }
+
+    ScreensManager sm = ScreensManager.get();
+    if(sm != null)
+    {
+      sm.call("home");
+    }
+    
+  }
+
+  public void act_ingame_retry()
+  {
+    ArenaSnakeManager.get().restart();
+  }
+
+}
---
+++ ScreensManager.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreensManager : EngineObject {
+  
+  protected ScreenObject[] screens;
+
+  protected override void build()
+  {
+    base.build();
+
+    fetchData();
+
+    call("loading");
+
+    EngineManager.get().onLoadingDone += onLoaded;
+  }
+  
+  override protected void fetchData()
+  {
+    base.fetchData();
+    screens = GameObject.FindObjectsOfType<ScreenObject>();
+  }
+
+  protected ScreenObject getScreen(string nm)
+  {
+    for (int i = 0; i < screens.Length; i++)
+    {
+      if (screens[i].name.Contains(nm)) return screens[i];
+    }
+    return null;
+  }
+
+  protected void onLoaded()
+  {
+
+    //###
+
+    //some stuff to reinit at each start
+    ScreenChallenges.resetSelection();
+
+    //start game
+    ScreenObject.call_home();
+  }
+
+  public void call(ScreenObject sc)
+  {
+    call(sc.name);
+  }
+
+  public void call(string nm)
+  {
+
+    for (int i = 0; i < screens.Length; i++)
+    {
+      if (screens[i].name.Contains(nm)) screens[i].show();
+      else screens[i].hide();
+    }
+
+  }
+
+  [ContextMenu("kill all")]
+  public void killAll()
+  {
+    fetchData();
+
+    call("none");
+  }
+
+  [ContextMenu("show all")]
+  public void showAll()
+  {
+    fetchData();
+
+    for (int i = 0; i < screens.Length; i++)
+    {
+      screens[i].showAll();
+    }
+
+  }
+
+  static public ScreensManager get() { return GameObject.FindObjectOfType<ScreensManager>(); }
+}
