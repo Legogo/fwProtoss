1284ad5e1013558c000f8e42943bcdbecdebb6b2
ab <andre.berlemont@gmail.com>
2017-10-11 13:55:43 +0200

    version has sdk mention

--- Version.cs
+++ Version.cs
@@ -21,7 +21,12 @@ public class Version : MonoBehaviour {
 #if UNITY_EDITOR
 
   public Text txt;
-  
+
+  private void Awake()
+  {
+    applyVersion();
+  }
+
   [MenuItem("Version/Increase Minor")]
   static protected void increase_minor()
   {
@@ -46,24 +51,39 @@ public class Version : MonoBehaviour {
     v.applyVersion();
   }
 
+  protected string formatedVersion()
+  {
+    string str = major + "." + minor + "." + version + "-" + build;
+#if sdk
+    str += "-sdk";
+#endif
+    return str;
+  }
+
   protected void applyVersion() {
 
     if (txt == null) txt = GetComponent<Text>();
     //https://mogutan.wordpress.com/2015/03/06/confusing-unity-mobile-player-settings-for-versions/
 
-    PlayerSettings.bundleVersion = major + "." + minor + "." + version + "-" + build;
+#if UNITY_EDITOR
+    PlayerSettings.bundleVersion = formatedVersion();
     PlayerSettings.Android.bundleVersionCode = build;
     PlayerSettings.iOS.buildNumber = PlayerSettings.bundleVersion;
-
-    if (txt != null)
-    {
-      txt.text = "" + PlayerSettings.bundleVersion;
-    }
-
-    Debug.Log("<b>DIRTY</b> "+PlayerSettings.bundleVersion);
+    
+    Debug.Log("<b>DIRTY</b> " + PlayerSettings.bundleVersion);
     EditorSceneManager.MarkSceneDirty(UnityEngine.SceneManagement.SceneManager.GetActiveScene());
-  }
-  
+
+    if (txt != null) txt.text = PlayerSettings.bundleVersion.ToString();
+
+#else
+    
+    if (txt != null) txt.text = formatedVersion();
+
 #endif
-  
+
+
+  }
+
+
+
 }
