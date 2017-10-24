8706e2f2d588d57edb59ff0fee1bb554f011eab7
Andre Berlemont <andre.berlemont@gmail.com>
2017-10-24 13:50:59 +0200

    settings manager

---
+++ ScriptableObjects/DataSettings.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+[CreateAssetMenu(menuName = "data/new DataSettings", order = 100)]
+public class DataSettings : ScriptableObject
+{
+  public string compagny_name = "playwing";
+  public string product_name = "Snake Smash";
+  public string package_name = "com.playwing.snakesmash";
+  
+  #if UNITY_EDITOR
+  public UIOrientation orientation_default = UIOrientation.Portrait;
+#endif
+
+  //public bool use_ingame_coin_rwd_video = true;
+  public bool use_joystick_visualisation = true;
+  
+}
---
+++ SettingsManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class SettingsManager : EngineObject {
+
+  public DataSettings data_settings;
+
+  static public SettingsManager get() {
+    return GameObject.FindObjectOfType<SettingsManager>();
+  }
+
+  public override void onEngineSceneLoaded()
+  {
+    base.onEngineSceneLoaded();
+    
+    TouchController tc = GameObject.FindObjectOfType<TouchController>();
+    if(tc != null) {
+      tc.useJoyVisualisation = data_settings.use_joystick_visualisation;
+    }
+  }
+
+#if UNITY_EDITOR
+  [ContextMenu("apply")]
+  public void apply() {
+    PlayerSettings.productName = data_settings.product_name;
+    PlayerSettings.companyName = data_settings.compagny_name;
+    
+    PlayerSettings.SetApplicationIdentifier(BuildTargetGroup.Android, data_settings.package_name);
+    PlayerSettings.SetApplicationIdentifier(BuildTargetGroup.iOS, data_settings.package_name);
+    
+    PlayerSettings.defaultInterfaceOrientation = data_settings.orientation_default;
+  }
+#endif
+}
