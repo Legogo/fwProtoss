7695e8cffe1bec960f7acc5e47bb6d4b0cdcb122
ab <andre.berlemont@gmail.com>
2017-11-09 15:18:38 +0100

    sound

---
+++ ScriptableObjects/DataSounds.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "data/new DataSounds", order = 100)]
+public class DataSounds : ScriptableObject {
+  public AudioClip[] clips;
+}
---
+++ SoundCarry.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SoundCarry : MonoBehaviour
+{
+  public DataSounds sounds;
+  public AudioMixer mixer;
+
+  private void Awake()
+  {
+    for (int i = 0; i < sounds.clips.Length; i++)
+    {
+      GameObject carry = new GameObject(sounds.clips[i].name);
+      AudioSource src = carry.AddComponent<AudioSource>();
+      src.clip = sounds.clips[i];
+      src.outputAudioMixerGroup = mixer.outputAudioMixerGroup;
+    }
+  }
+}
