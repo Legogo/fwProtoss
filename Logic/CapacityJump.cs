0ee90252d9a52a0bb50e1e97d0dda5f2bf10629a
ab <andre.berlemont@gmail.com>
2018-01-31 13:12:51 +0100

    dependencies

--- Logic/CapacityJump.cs
+++ Logic/CapacityJump.cs
@@ -9,19 +9,19 @@ abstract public class CapacityJump : LogicCapacity
   CapacityCollision _collision;
   HiddenCapacityMovement _move;
 
-  HiddenCapacityPlayerInput _input;
-  
+  InputKeyTopDown _input;
+
   public override void setupCapacity()
   {
     _collision = _owner.GetComponent<CapacityCollision>();
     _move = _owner.GetComponent<HiddenCapacityMovement>();
-    _input = _owner.GetComponent<HiddenCapacityPlayerInput>();
+    _input = _owner.GetComponent<CapacityInput>().keys.get<InputKeyTopDown>();
   }
 
   public override void updateLogic()
   {
     //jump when snapped on wall
-    if (_input.keys.get<InputKeyTopDown>().pressed_jump())
+    if (_input.pressed_jump())
     {
       solveJump();
     }
