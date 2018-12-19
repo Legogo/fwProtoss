8a19427d4651b43779090228f784947a6d79fa50
Andre Berlemont <andre.berlemont@gmail.com>
2018-12-19 21:09:03 +0100

    rewrote chrono and timer

---
+++ Time/TimeObject.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+abstract public class TimeObject
+{
+  protected DataTimer balancingTimer;
+  bool running = false;
+
+  float time = 0f;
+
+  virtual public void setupBalancing(DataTimer data)
+  {
+    balancingTimer = data;
+  }
+  
+  virtual public void reset()
+  {
+    time = 0f;
+  }
+
+  virtual public void play(bool resetTime = true)
+  {
+    running = true;
+
+    if (resetTime) reset();
+  }
+
+  virtual public void pause()
+  {
+    running = false;
+  }
+
+  virtual public void stop()
+  {
+    pause();
+    reset();
+  }
+
+  virtual public void update()
+  {
+    if (!isRunning()) return;
+
+    float mul = 1f;
+
+#if debug
+    //debug
+    if (Input.GetKey(KeyCode.P)) mul = 10f;
+#endif
+
+    time += Time.deltaTime * mul;
+  }
+
+  public bool hasBalancing() { return balancingTimer != null; }
+
+  public float getTime() { return time; }
+
+  public bool isRunning() { return running; }
+
+  abstract public float getTimeRemaining();
+  
+}
