17fafbfaf54357d44e5cd59d090eaa2ab9fa65d3
ab <andre.berlemont@gmail.com>
2019-01-23 16:06:10 +0100

    process process

--- Halpers/HalperCoroutine.cs
+++ Halpers/HalperCoroutine.cs
@@ -16,4 +16,24 @@ static public class HalperCoroutine {
 
   }
 
+  /// <summary>
+  /// to run a list of enumerator until all are done running
+  /// </summary>
+  /// <param name="processList"></param>
+  /// <returns></returns>
+  static public IEnumerator processProcess(List<IEnumerator> processList)
+  {
+    while(processList.Count > 0)
+    {
+      int idx = 0;
+      while(idx < processList.Count)
+      {
+        if (!processList[idx].MoveNext()) processList.RemoveAt(idx);
+        else idx++;
+      }
+
+      //Debug.Log("  L process count : " + processList.Count);
+      yield return null;
+    }
+  }
 }
