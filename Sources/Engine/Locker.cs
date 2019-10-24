0834ac84185f63b8b3b9de6422940ff4e9b0a710
aberlemont <andre.berlemont@gmail.com>
2019-10-24 21:45:00 +0200

    locker

---
+++ Sources/Engine/Locker.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Locker
+{
+  List<MonoBehaviour> locks = new List<MonoBehaviour>();
+
+  public Locker()
+  {
+
+  }
+
+  public void remove<T>() where T : MonoBehaviour
+  {
+    int i = 0;
+    while(i < locks.Count)
+    {
+      if(locks[i] as T)
+      {
+        locks.RemoveAt(i);
+        continue;
+      }
+
+      i++;
+    }
+  }
+
+  public bool has(MonoBehaviour obj)
+  {
+    return locks.IndexOf(obj) > -1;
+  }
+
+  public void add(MonoBehaviour obj)
+  {
+    if (locks.IndexOf(obj) < 0) locks.Add(obj);
+  }
+
+  public void remove(MonoBehaviour obj)
+  {
+    if (locks.IndexOf(obj) > -1) locks.Remove(obj);
+  }
+
+  public bool isLocked()
+  {
+    return locks.Count > 0;
+  }
+}
